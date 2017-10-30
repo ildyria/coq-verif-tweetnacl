fb33188ca2ef0b7a0841f0324b423c81eedeb168
Benoit Viguier <b.viguier@cs.ru.nl>
2017-10-30 15:48:40 +0100

    add M

---
+++ Low/M.v
@@ -0,0 +1,15 @@
+Require Import Tweetnacl.Libs.Export.
+Require Export Tweetnacl.Low.M_low_level_compute.
+Require Export Tweetnacl.Low.Inner_M1.
+Require Export Tweetnacl.Low.Outer_M1.
+Require Export Tweetnacl.Mid.M_low_level.
+Require Export Tweetnacl.Mid.M.
+Require Export Tweetnacl.Mid.ScalarMult.
+Require Export Tweetnacl.Low.Car25519.
+
+
+Definition M (a b o : list Z) : list Z := (car25519 (car25519 (M3_fix (Z.of_nat 16%nat)
+                  (M2_fix (Z.of_nat 15%nat)
+                    (M1_fix a b)
+                  )
+                  o))).
