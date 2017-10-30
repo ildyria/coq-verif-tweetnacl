48d4e576684657dff9ffceb65230503d58e68555
Benoit Viguier <b.viguier@cs.ru.nl>
2017-10-30 17:19:46 +0100

    update M.v

--- Low/M.v
+++ Low/M.v
@@ -6,6 +6,7 @@ Require Export Tweetnacl.Mid.M_low_level.
 Require Export Tweetnacl.Mid.M.
 Require Export Tweetnacl.Mid.ScalarMult.
 Require Export Tweetnacl.Low.Car25519.
+Require Export Tweetnacl.Mid.Car25519.
 
 
 Definition M (a b o : list Z) : list Z := (car25519 (car25519 (M3_fix (Z.of_nat 16%nat)
