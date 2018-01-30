03835325fc9e3c94e969d4e26d360fd5ca03376e
benoit <b.viguier@cs.ru.nl>
2018-01-30 16:26:50 +0100

    typo...

--- Low/Crypto_Scalarmult.v
+++ Low/Crypto_Scalarmult.v
@@ -7,7 +7,7 @@ From Tweetnacl.Low Require Import Get_abcdef.
 From Tweetnacl.Low Require Import Constant.
 From Tweetnacl.Low Require Import Prep_n.
 
-Definition Crypot_Scalarmult n p :=
+Definition Crypto_Scalarmult n p :=
   let a := get_a (montgomery_fn 255 254 (clamp n) One16 (Unpack25519 p) nul16 One16 nul16 nul16 (Unpack25519 p)) in
   let c := get_c (montgomery_fn 255 254 (clamp n) One16 (Unpack25519 p) nul16 One16 nul16 nul16 (Unpack25519 p)) in
   Pack25519 (M a (Inv25519 c)).
