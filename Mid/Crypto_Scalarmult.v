eb89aab11803f14ab4e3fa7763100483f64ced9d
Benoit Viguier <b.viguier@cs.ru.nl>
2018-10-24 14:12:12 +0200

    more work

--- Mid/Crypto_Scalarmult.v
+++ Mid/Crypto_Scalarmult.v
@@ -1,18 +1,21 @@
 From Tweetnacl Require Import Libs.Export.
 From Tweetnacl Require Import Gen.Get_abcdef.
 From Tweetnacl Require Import Gen.AMZubSqSel.
+From Tweetnacl Require Import Gen.AMZubSqSel_Prop.
 From Tweetnacl Require Import Gen.abstract_fn_rev.
 From Tweetnacl Require Import Gen.montgomery_rec.
 From Tweetnacl Require Import Gen.montgomery_rec_eq.
 From Tweetnacl Require Import Mid.AMZubSqSel.
 From Tweetnacl Require Import Mid.Reduce.
 From Tweetnacl Require Import Mid.GetBit.
+From Tweetnacl Require Import Mid.GetBit_bitn.
 From Tweetnacl Require Import Mid.Prep_n.
 From Tweetnacl Require Import Mid.Unpack25519.
 From Tweetnacl Require Import Mid.Pack25519.
 From Tweetnacl Require Import Mid.Car25519.
 From Tweetnacl Require Import Mid.Inv25519.
 From Tweetnacl Require Import Mid.ScalarMult.
+From Tweetnacl Require Import Mid.Crypto_Scalarmult_Fp.
 
 From Tweetnacl.High Require Import Zmodp opt_ladder curve25519.
 From mathcomp Require Import ssreflect ssrbool eqtype ssralg.
@@ -32,7 +35,6 @@ Definition ZCrypto_Scalarmult_rec_gen n p :=
   ZPack25519 (Z.mul (get_a t) (ZInv25519 (get_c t))).
 
 End ZCrypto_Scalarmult_gen.
-
 
 Local Instance Z_Ops : (Ops Z Z (fun x => Z.modulo x (Z.pow 2 255 - 19))) := {}.
 Proof.
@@ -76,7 +78,7 @@ Proof.
   reflexivity.
 Qed.
 
-(* Proof of correctness between High and Mid Level *)
+(* (* Proof of correctness between High and Mid Level *)
 Lemma A_ok (a b : Z) : Zmodp.pi (A a b) = (Zmodp.pi a + Zmodp.pi b)%R.
 Proof. by rewrite Zmodp_addE. Qed.
 
@@ -93,9 +95,79 @@ Qed.
 
 Lemma Sq_ok (a : Z) : Zmodp.pi (Sq a) = (Zmodp.pi a ^+ 2)%R.
 Proof. by rewrite /Sq M_ok GRing.expr2. Qed.
+ *)
 
-Lemma ZCrypto_Scalarmult_curve25519_ladder n (x : Zmodp.type) :
-  ZCrypto_Scalarmult (Z.of_nat n) (val x) = val (curve25519_ladder n x).
-Proof. Admitted.
+Local Instance Z25519_Ops : (Ops Zmodp.type nat id) := {}.
+Proof.
+apply Zmodp.add.
+apply Zmodp.mul.
+apply (fun x y => Zmodp.add x (Zmodp.opp y)).
+apply (fun x => Zmodp.mul x x).
+apply Zmodp.zero.
+apply Zmodp.one.
+apply (Zmodp.pi C_121665).
+apply (fun b p q => if b =? 0 then p else q).
+apply (fun n m => Z.of_nat (ladder.bitn (Z.to_nat (Z.of_nat m)) (Z.to_nat n))).
+reflexivity.
+reflexivity.
+reflexivity.
+reflexivity.
+reflexivity.
+reflexivity.
+Defined.
+
+(* Local Instance Z_Ops : (Ops Z Z Mod) := {}.
+Proof.
+apply A.
+apply M.
+apply Zub.
+apply Sq.
+apply C_0.
+apply C_1.
+apply C_121665.
+apply Sel25519.
+apply Zgetbit.
+intros b p q ; rewrite /Sel25519 ; flatten.
+intros ; apply A_mod_eq.
+intros ; apply M_mod_eq.
+intros ; apply Zub_mod_eq.
+intros ; apply Sq_mod_eq.
+intros ; apply Zmod_mod.
+Defined.
+ *)
+
+Local Instance Z25519_Z_Eq : @Ops_Mod_P Zmodp.type nat Z Mod id Z25519_Ops Z_Ops := {
+P := val;
+P' := Z.of_nat
+}.
+Proof.
+intros; simpl. rewrite /A /Mod /p -lock Zmod_mod. reflexivity.
+intros; simpl. rewrite /M /Mod /p -lock Zmod_mod -Zcar25519_correct -Zcar25519_correct. reflexivity.
+intros; simpl. rewrite /Zub /Mod /p -lock Zmod_mod.
+rewrite -Zminus_mod_idemp_l.
+change ((2 ^ 255 - 19) :𝓖𝓕) with 0%Z.
+rewrite Zplus_mod_idemp_r.
+f_equal.
+intros; simpl. rewrite /Sq /M /Mod /p -lock Zmod_mod -Zcar25519_correct -Zcar25519_correct. reflexivity.
+simpl; rewrite /C_121665 /p -lock. reflexivity.
+simpl; rewrite /C_0 /p -lock. reflexivity.
+simpl; rewrite /C_1 /p -lock. reflexivity.
+intros; simpl.
+rewrite /Mod /Sel25519 ; flatten.
+intros; simpl.
+apply Zgetbit_bitn.
+Defined.
+
+SearchAbout Z Zmodp.type.
+Lemma ZCrypto_Scalarmult_curve25519_ladder n x : (*(x : Zmodp.type) :*)
+  ZCrypto_Scalarmult n x = val (curve25519_ladder (Z.to_nat (Zclamp n)) (Zmodp.pi x)).
+Proof.
+rewrite /ZCrypto_Scalarmult.
+rewrite /curve25519_ladder.
+rewrite /opt_montgomery.
+rewrite /ZCrypto_Scalarmult_rev_gen.
+rewrite /ZPack25519.
+rewrite /ZInv25519.
+Admitted.
 
 Close Scope Z.
