8d2303d42c87bf33057acfeb84a86625db946e22
Benoit Viguier <b.viguier@cs.ru.nl>
2019-08-12 20:05:45 +0200

    more rewrite

--- proofs/spec/Gen/montgomery_rec.v
+++ proofs/spec/Gen/montgomery_rec.v
@@ -13,6 +13,11 @@ Context {O : Ops T T' Mod}.
 
 Open Scope Z.
 
+Local Notation "X + Y" := (A X Y) (only parsing).
+Local Notation "X - Y" := (Zub X Y) (only parsing).
+Local Notation "X * Y" := (M X Y) (only parsing).
+Local Notation "X ^2" := (Sq X) (at level 40, only parsing, left associativity).
+
 Fixpoint montgomery_rec (m : nat) (z : T') (a b c d e f x : T) : (T * T * T * T * T * T) :=
   match m with
   | 0%nat => (a,b,c,d,e,f)
@@ -20,24 +25,24 @@ Fixpoint montgomery_rec (m : nat) (z : T') (a b c d e f x : T) : (T * T * T * T 
       let r := Getbit (Z.of_nat n) z in
       let (a, b) := (Sel25519 r a b, Sel25519 r b a) in
       let (c, d) := (Sel25519 r c d, Sel25519 r d c) in
-      let e := A a c in
-      let a := Zub a c in
-      let c := A b d in
-      let b := Zub b d in
-      let d := Sq e in
-      let f := Sq a in
-      let a := M c a in
-      let c := M b e in
-      let e := A a c in
-      let a := Zub a c in
-      let b := Sq a in
-      let c := Zub d f in
-      let a := M c C_121665 in
-      let a := A a d in
-      let c := M c a in
-      let a := M d f in
-      let d := M b x in
-      let b := Sq e in
+      let e := a + c in
+      let a := a - c in
+      let c := b + d in
+      let b := b - d in
+      let d := e ^2 in
+      let f := a ^2 in
+      let a := c * a in
+      let c := b * e in
+      let e := a + c in
+      let a := a - c in
+      let b := a ^2 in
+      let c := d - f in
+      let a := c * C_121665 in
+      let a := a + d in
+      let c := c * a in
+      let a := d * f in
+      let d := b * x in
+      let b := e ^2 in
       let (a, b) := (Sel25519 r a b, Sel25519 r b a) in
       let (c, d) := (Sel25519 r c d, Sel25519 r d c) in
       montgomery_rec n z a b c d e f x
